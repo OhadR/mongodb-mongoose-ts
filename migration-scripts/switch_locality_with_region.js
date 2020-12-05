aecbb2cc2cc419247deee5d9f84295514896e65c
OhadR <ohadr.developer@gmail.com>
2020-12-05 22:06:27 +0200

    improve query

--- migration-scripts/switch_locality_with_region.js
+++ migration-scripts/switch_locality_with_region.js
@@ -11,25 +11,20 @@ const totalDocCount = db.getCollection('sessions').find().count();
 print(`totalDocCount: [${totalDocCount}]`);
 
 //Find the first document in the customers collection:
-const result = db.getCollection('sessions').find().toArray();
+const result = db.getCollection('sessions').find({'session.metadata.area_locality' : 'usa'}).toArray();
 
 let i = 0;
 
 //    console.log(result);
 for(session of result) {
-	if(session.metadata.area_locality === 'usa') {
-		const tmp = session.metadata.area_region;
-		var newvalues = {
-			$set: {
-				'metadata.area_locality': tmp,
-				'metadata.area_region': 'usa'
-			}};
-		db.getCollection("sessions").updateOne({_id: session._id}, newvalues);
-		print("documents updated: ", ++i);
-	}
-	else {
-		print("documents NOT updated: ");
-	}
+	const tmp = session.metadata.area_region;
+	var newvalues = {
+		$set: {
+			'metadata.area_locality': tmp,
+			'metadata.area_region': 'usa'
+		}};
+	db.getCollection("sessions").updateOne({_id: session._id}, newvalues);
+	print("documents updated: ", ++i);
 }
 
 //	db.close();
